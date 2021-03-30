6b02f115046477c9bd0c40cffac25e5b2aa7ad31
amir <alsadi.amir@gmail.com>
2021-03-30 17:16:43 +0200

    added test alogs folder

---
+++ test_algorithms/Controller_in_the_loop/SendT_header.py
@@ -0,0 +1,15 @@
+
+from scapy.all import *
+import sys, os
+
+TYPE_SENDTS = 0x1234
+TYPE_IPV4 = 0x0800
+
+
+class SendT(Packet):
+    name = "OpProgram"
+    fields_desc = [StrFixedLenField("P", "T", length=1),
+                   LongField("ts",long(time.time()*1000))]
+
+bind_layers(Ether, SendT, type=TYPE_SENDTS)
+bind_layers(SendT, IP, pid=TYPE_IPV4)
---
+++ test_algorithms/Controller_in_the_loop/sendT.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+for i in {0..50}
+do
+  ./send_T.py
+done
